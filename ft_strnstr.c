c9a3803eecb3d79cf546497fcff8a852ef8b72e1
Anas Jaidi <ajaidi@e1r9p12.1337.ma>
2021-11-02 17:50:38 +0100

    add strnstr and fix some norminette prblms

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,23 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ajaidi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/02 17:06:48 by ajaidi            #+#    #+#             */
-/*   Updated: 2021/11/02 17:06:54 by ajaidi           ###   ########.fr       */
+/*   Created: 2021/11/02 17:21:47 by ajaidi            #+#    #+#             */
+/*   Updated: 2021/11/02 17:22:42 by ajaidi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_memcmp(const void *s1, const void *s2, size_t n)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
+	int		j;
 
 	i = 0;
-	if (!n)
-		return (0);
-	while (*(char *)(s1 + i) == *(char *)(s2 + i) && i < n - 1)
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] == needle[j] && i + j < len)
+		{
+			if (needle[j + 1] == '\0')
+				return ((char *)(haystack + i));
+		}
 		i++;
-	return (*(char *)(s1 + i) - *(char *)(s2 + i));
+	}
+	return (NULL);
 }
