5ff380f871c5f3ab8e5cc8b5633a530a01996761
Anas Jaidi <ajaidi@e1r9p12.1337.ma>
2021-11-02 12:04:40 +0100

    add ft_strlcat.c for testing

---
+++ ft_strlcat.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ajaidi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/02 11:49:07 by ajaidi            #+#    #+#             */
+/*   Updated: 2021/11/02 11:49:08 by ajaidi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+size_t	strlcat(char * dst, const char * src, size_t dstsize)
+{
+	size_t	d;
+	size_t	s;
+	size_t	r;
+
+	d = 0;
+	s = 0;
+	while (dst[d])
+		d++;
+	while (src[s])
+		s++;
+	if (d >= dstsize)
+		r = s + dstsize;
+	else
+		r = d + s;
+	if (dstsize)
+	{
+		while (*src && d < dstsize - 1)
+			*dst++ = *src++;
+	}
+	return (r);
+}
