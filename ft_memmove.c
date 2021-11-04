297462066fd11e1b90f8b5c55c7f6cc6046ff464
Anas Jaidi <ajaidi@e1r4p14.1337.ma>
2021-11-04 15:18:31 +0100

    add all functions of part 1 with norminette errors in memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,6 +6,25 @@
 /*   By: ajaidi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 11:14:50 by ajaidi            #+#    #+#             */
-/*   Updated: 2021/11/03 15:40:32 by ajaidi           ###   ########.fr       */
+/*   Updated: 2021/11/04 15:16:23 by ajaidi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	 size_t	i;
+
+	 i = 0;
+	 if (dst > src)
+	 {
+		len++;
+	 	while (--len)
+	 		*(char *)(dst + len) = *(char *)(src + len):
+	 }
+	 else if (src > dst)
+	 {
+		 while (++i < len)
+			 *(char *)(dst + i) = *(char *)(src + i);
+	 }
+	 return (dst);
+}
