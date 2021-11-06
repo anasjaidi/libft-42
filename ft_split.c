225dbcc785c54bfe8aa95842649ee2f9ced93d90
Anas Jaidi <anas.jaidi@icloud.com>
2021-11-06 18:48:16 +0100

    add split for test

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: ajaidi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 10:46:31 by ajaidi            #+#    #+#             */
-/*   Updated: 2021/11/06 18:39:50 by ajaidi           ###   ########.fr       */
+/*   Updated: 2021/11/06 18:48:08 by ajaidi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 int	ft_count(char *str, char c)
 {
 	int	i;
-	int	c;
+	int	c1;
 
 	i = -1;
-	c = 0;
+	c1 = 0;
 	while (str[++i])
 		if ((str[i] != c && str[i] == c) || (str[i] != c && str[i + 1] == '\0'))
-			c++;
-	return (c);
+			c1++;
+	return (c1);
 }
 
 char	*ft_word(int start, int end, char *str)
@@ -37,7 +37,7 @@ char	*ft_word(int start, int end, char *str)
 	if (!r)
 		return (NULL);
 	while (start < end)
-		r[++i] = str[start++]
+		r[++i] = str[start++];
 	r[size] = 0;
 	return (r);
 }
@@ -47,9 +47,11 @@ char	**ft_splt(char *str, char sep, int size)
 	int		i;
 	int		j;
 	char	**r;
+	int		d;
 
 	i = 0;
 	j = 0;
+	d = -1;
 	r = (char **)malloc((size + 1) * sizeof(char *));
 	if (!r)
 		return (NULL);
@@ -74,7 +76,7 @@ char	**ft_split(char const *s, char c)
 	char	**r;
 	int		size;
 
-	size = ft_count(s, c);
+	size = ft_count((char *)s, c);
 	if (c == '\0')
 	{
 		r = (char **)malloc(2 * sizeof(char *));
@@ -82,6 +84,6 @@ char	**ft_split(char const *s, char c)
 		r[1] = 0;
 		return (r);
 	}
-	r = ft_splt(s, c, size);
+	r = ft_splt((char *)s, c, size);
 	return (r);
 }
